db601c22e7bd5057684c6bd4744a3c394387af08
YinHk <ministudiowingyin@gmail.com>
2020-09-19 23:29:39 +0900

    Updates

---
+++ precache-manifest.a8566d0bc9058fc0e6aef53cc0cdab66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33faf9f128bdf7d6a6b8e676ceec6c0",
+    "url": "/currency_converter_app/index.html"
+  },
+  {
+    "revision": "88776df1f6f5f3e526f6",
+    "url": "/currency_converter_app/static/css/main.4e158bfe.chunk.css"
+  },
+  {
+    "revision": "0bc5cdd22fa77e93a87d",
+    "url": "/currency_converter_app/static/js/2.700e1c33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency_converter_app/static/js/2.700e1c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88776df1f6f5f3e526f6",
+    "url": "/currency_converter_app/static/js/main.eb433df2.chunk.js"
+  },
+  {
+    "revision": "ac7171df2b8482379668",
+    "url": "/currency_converter_app/static/js/runtime-main.bc2ad45b.js"
+  },
+  {
+    "revision": "18736818d973b07a699d2bc21e70ee37",
+    "url": "/currency_converter_app/static/media/arrow.18736818.png"
+  }
+]);
